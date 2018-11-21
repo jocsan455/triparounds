ed1e2edbe42885175e145cc7f02e706e3dcb18c7
jocsan455 <jocsan455@gmail.com>
2018-11-20 23:13:25 -0430

    primer commit

---
+++ assets/js/custom2.js
@@ -0,0 +1,162 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        /*---------------------------------------------------
+            project filter
+        ----------------------------------------------------*/
+        var Container = $('#project-area');
+        Container.imagesLoaded(function () {
+            var portfolio = $('.project-menu');
+            portfolio.on('click', 'button', function () {
+                $(this).addClass('active').siblings().removeClass('active');
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+            var $grid = $('.project-items').isotope({
+                itemSelector: '.grid-item',
+                masonry: {
+                    columnWidth: 1
+                }
+            });
+
+        });
+
+        /*---------------------------------------------------
+            home slider
+        ----------------------------------------------------*/
+        $('.main-slider').owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            smartSpeed: 450,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                991: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            testimonial carousel
+        ----------------------------------------------------*/
+        $('.testimonial-carousel').owlCarousel({
+            loop: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                991: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            partner carousel
+        ----------------------------------------------------*/
+        $('.partner-carousel').owlCarousel({
+            loop: true,
+            autoplay: true,
+            margin: 20,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                991: {
+                    items: 4
+                },
+                1200: {
+                    items: 6
+                },
+                1920: {
+                    items: 6
+                }
+            }
+        });
+        /*---------------------------------------------------
+            counter
+        ----------------------------------------------------*/
+        $('.counter-single h2').counterUp({
+            delay: 10, // the delay time in ms
+            time: 1000 // the speed time in ms
+        });
+
+
+        /*---------------------------------------------------
+                popUp video
+        ----------------------------------------------------*/
+        $('.popup-video').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false,
+            disableOn: 300
+        });
+
+        /*---------------------------------------------------
+            scrollIt plugin activation
+        ----------------------------------------------------*/
+        $.scrollIt();
+
+        /*---------------------------------------------------
+            smooth scroll
+        ----------------------------------------------------*/
+        $('a[href*="#"]:not([href="#"]):not([data-toggle])').on('click', function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            accordian
+        ----------------------------------------------------*/
+        $('.collapse').on('shown.bs.collapse', function () {
+            $(this).prev().addClass('active');
+        });
+
+        $('.collapse').on('hidden.bs.collapse', function () {
+            $(this).prev().removeClass('active');
+        });
+
+
+    });
+
+}(jQuery));
